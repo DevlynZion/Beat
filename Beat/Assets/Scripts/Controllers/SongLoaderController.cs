8d8a4a8ebf9a8db6a779b740d904d6cdbeb3c268
DevlynZion <DevlynZion@users.noreply.github.com>
2021-01-27 21:27:23 +0200

    XML Songs

--- Beat/Assets/Scripts/Controllers/SongLoaderController.cs
+++ Beat/Assets/Scripts/Controllers/SongLoaderController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
 using UnityEngine;
 
 public class SongLoaderController : MonoBehaviour
@@ -51,23 +53,68 @@ public class SongLoaderController : MonoBehaviour
     {
         var songData = new SongData();
 
-        var path = Application.persistentDataPath + SongName + ".bin";
-        songData.SongName = SongName;
+        var song = new SongData();
 
-        if (File.Exists(path))
+        var filePath = Path.Combine(Application.streamingAssetsPath, string.Format("Songs/{0}.xml", SongName));
+
+        song.SongName = SongName;
+
+        using (var www = new WWW(filePath))
         {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
+            XmlDocument xml = null;
+            while (!www.isDone) { }
 
-            SongData data = formatter.Deserialize(stream) as SongData;
-            stream.Close();
+            xml = new XmlDocument();
+            xml.LoadXml(www.text);
 
-            return data;
+            var songNode = xml.DocumentElement.SelectSingleNode("/song");
+
+            var songAttributes = songNode.Attributes;
+
+            songData.SongName = songAttributes["name"].Value;
+
+            int bpm;
+            if (int.TryParse(songAttributes["bpm"].Value, out bpm))
+                songData.BMP = bpm;
+
+            var barsNodes = songNode.SelectSingleNode("bars");
+            var barNodes = barsNodes.SelectNodes("bar");
+
+            foreach (XmlNode barNode in barNodes)
+            {
+                var bar = new BarData();
+                var beatNodes = barNode.SelectNodes("beat");
+
+                foreach (XmlNode beatNode in beatNodes)
+                {
+                    var beat = new BeatData();
+                    var beatAttributes = beatNode.Attributes;
+
+                    int beatNumber;
+                    if (int.TryParse(beatAttributes["beatNumber"].Value, out beatNumber))
+                        beat.BeatNumber = beatNumber;
+
+                    System.Globalization.NumberFormatInfo nf = new System.Globalization.NumberFormatInfo()
+                    {
+                        NumberGroupSeparator = "."
+                    };
+
+                    float yPosition;
+                    yPosition = float.Parse(beatAttributes["yPosition"].Value, nf);
+                    beat.YPosition = yPosition;
+
+
+                    beat.BeatPrephabName = beatAttributes["beatPrephabName"].Value;
+                    beat.HitAudioClipName = beatAttributes["hitAudioClipName"].Value;
+
+                    bar.Beats.Add(beat);
+                }
+
+                songData.Bars.Add(bar);
+            }
         }
-        else
-        {
-            return null;
-        }
+
+        return songData;
     }
 
     public void SaveSong(Song Song)
