0300512ffaa739fa46af7e54f0633a7aff363c9b
DevlynZion <DevlynZion@users.noreply.github.com>
2022-12-09 17:34:42 +0200

    Update controls for interview

--- Beat/Assets/Scripts/Controllers/PlayerController.cs
+++ Beat/Assets/Scripts/Controllers/PlayerController.cs
@@ -15,14 +15,22 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-        if(IsLongPlayer)
+        if (IsLongPlayer)
         {
             var boxCollider2D = GetComponent<BoxCollider2D>();
             boxCollider2D.size = new Vector2(boxCollider2D.size.x, 20);
         }
     }
 
+    // TODO: still trying to find good controls to use
     void FixedUpdate()
+    {
+        //TouchMove();
+        //ButtonMove();
+        KeybordMove();
+    }
+
+    private void ButtonMove()
     {
         //float mouseYvalue =  CrossPlatformInputManager.GetAxis("Vertical");
 
@@ -59,5 +67,37 @@ public class PlayerController : MonoBehaviour
         {
             playerRigidbody.velocity = Vector2.zero;
         }
+    }
+    private void TouchMove()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
+
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                // get the touch position from the screen touch to world point
+                Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                // lerp and set the position of the current object to that of the touch, but smoothly over time.
+                transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
+            }
+        }
+    }
+
+    private void KeybordMove()
+    {
+        var moveBy = Input.GetAxis("Vertical");
+
+        if (moveBy != 0.0f)
+        {
+            playerRigidbody.AddForce(new Vector2(0, moveBy * PlayerMoveForce), ForceMode2D.Impulse);
+
+            previousDirection = moveBy;
+        }
+        else
+        {
+            playerRigidbody.velocity = Vector2.zero;
+        }
+
     }
 }
